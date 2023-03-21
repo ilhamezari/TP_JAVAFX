07ba5f7e12631e65d09c4c7442736b882fdd7606
ilhamezari <119487198+ilhamezari@users.noreply.github.com>
2023-03-21 16:32:32 +0000

    Update db_stock.sql

--- TP-JAVAFXORM/db_stock.sql
+++ TP-JAVAFXORM/db_stock.sql
@@ -1,23 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Hôte : 127.0.0.1
--- Généré le : mar. 21 mars 2023 à 17:23
--- Version du serveur : 10.4.27-MariaDB
--- Version de PHP : 8.0.25
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
 -- Base de données : `db_stock`
 --
 
